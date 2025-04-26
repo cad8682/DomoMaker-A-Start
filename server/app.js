d97d31d090ac420290548bef70ec64c696e2f59a
Mozzy Stix <cad8682@g.rit.edu>
2025-04-26 17:45:39 -0400

    DomoA done

--- server/app.js
+++ server/app.js
@@ -15,6 +15,7 @@ const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+//Download Mongo
 const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
     if(err) {
